1bb52540076b08a319394f054d8dd9f7016b8db7
Gihoon <89381041+Qussong@users.noreply.github.com>
2024-08-26 09:49:56 +0900

    Inventory System 구현중

--- GamePortFolio/Source/GamePortFolio/Item/GHItemBase.cpp
+++ GamePortFolio/Source/GamePortFolio/Item/GHItemBase.cpp
@@ -2,11 +2,35 @@
 
 
 #include "Item/GHItemBase.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 
 AGHItemBase::AGHItemBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// DataTable Section
+	static ConstructorHelpers::FObjectFinder<UDataTable> 
+		ItemDataTableRef(TEXT("/Script/Engine.DataTable'/Game/Gihoon/Item/Data/DT_Inventory.DT_Inventory'"));
+	if (ItemDataTableRef.Succeeded())
+	{
+		ItemDataTable = ItemDataTableRef.Object;
+	}
+
+	// Collision Section
+	ItemCollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("ItemCollision"));
+	RootComponent = ItemCollisionComp;
+	ItemCollisionComp->SetSphereRadius(100.f);
+	ItemCollisionComp->SetCollisionProfileName(TEXT("ItemProfile"));
+
+	// StaticMesh Section
+	ItemStaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemStaticMesh"));
+	ItemStaticMeshComp->SetupAttachment(RootComponent);
+
+	// SkeletalMesh Section
+	ItemSkeletalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ItemSkeletalMesh"));
+	ItemSkeletalMeshComp->SetupAttachment(RootComponent);
 }
 
 void AGHItemBase::BeginPlay()
@@ -19,5 +43,9 @@ void AGHItemBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AGHItemBase::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 }
 
--- GamePortFolio/Source/GamePortFolio/Item/GHItemBase.h
+++ GamePortFolio/Source/GamePortFolio/Item/GHItemBase.h
@@ -20,4 +20,32 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemComponent")
+	TObjectPtr<class USphereComponent> ItemCollisionComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemComponent")
+	TObjectPtr<class UStaticMeshComponent> ItemStaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemComponent")
+	TObjectPtr<class USkeletalMeshComponent> ItemSkeletalMeshComp;
+
+// Collision Overlap Section
+protected:
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+// Data Section
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable)
+	TObjectPtr<UDataTable> ItemDataTable;
+	
+public:
+
+
 };
